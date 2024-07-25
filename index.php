cc6a152a90bf0c48aae3af5719c69741ba0cf441
Hector Valdiviezo <hectomar02@hotmail.es>
2024-07-24 22:15:43 -0500

    Añadiendo compras al dashboard y creando módulo Ventas

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include ('app/controllers/roles/listado_de_roles.php');
 include ('app/controllers/categorias/listado_de_categorias.php');
 include ('app/controllers/almacen/listado_de_productos.php');
 include ('app/controllers/proveedores/listado_de_proveedores.php');
+include ('app/controllers/compras/listado_de_compras.php');
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -151,8 +152,31 @@ include ('app/controllers/proveedores/listado_de_proveedores.php');
                             
                         </div>
                     </div>
-                
-                </div>
+
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <?php
+                                $contador_de_compras = 0;
+                                foreach ($compras_datos as $compras_dato){
+                                    $contador_de_compras = $contador_de_compras + 1;
+                                }
+                                ?>
+                                <h3><?php echo $contador_de_compras;?></h3>
+                                <p>Compras Registradas</p>
+                            </div>
+                            <a href="<?php echo $URL;?>/compras/create.php">
+                                <div class="icon">
+                                    <i class="fas fa-cart-plus"></i>
+                                </div>
+                            </a>
+                            <a href="<?php echo $URL;?>/compras" class="small-box-footer">
+                                Más detalles <i class="fas fa-arrow-circle-right"></i>
+                            </a>
+                            
+                        </div>
+                    </div>
+            </div>
         </div>
         <!-- /.container-fluid -->
     </div>
